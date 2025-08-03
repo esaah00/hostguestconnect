29c3b2c8788126f179277b14e35849b0e4091265
Safe00 <aymadrid007@gmail.com>
2025-08-03 21:29:41 +0200

    Add

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 const About: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 pt-16">
-      <Navbar onSearch={() => {}} />
+      <Navbar  />
       <div className="max-w-7xl mx-auto py-6 px-4">
         <h1 className="text-3xl font-bold text-gray-900">About Us</h1>
         <p className="mt-4 text-gray-600">
